be13e63a3f7b0ecf057dbaad958d8e9169c2ac3e
DysaniazzZ <William.fzy@foxmail.com>
2017-07-04 18:11:33 +0800

    chapter05: remoteviews usage in multi processes

--- chapter05/src/main/java/com/dysania/artofandroid/chapter05/DemoActivity.java
+++ chapter05/src/main/java/com/dysania/artofandroid/chapter05/DemoActivity.java
@@ -1,6 +1,15 @@
 package com.dysania.artofandroid.chapter05;
 
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Process;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.RemoteViews;
 
 /**
  * Created by DysaniazzZ on 03/07/2017.
@@ -8,4 +17,35 @@ import android.support.v7.app.AppCompatActivity;
 
 public class DemoActivity extends AppCompatActivity {
 
+    public static final String ACTION_REMOTE_VIEWS = "com.dysania.artofandroid.chapter05.action_REMOTE";
+    public static final String EXTRA_REMOTE_VIEWS = "extra_remote_views";
+
+    public static void actionStart(Context context) {
+        Intent intent = new Intent(context, DemoActivity.class);
+        context.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo);
+        findViewById(R.id.btn_mock).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeMockNotification();
+            }
+        });
+    }
+
+    private void changeMockNotification() {
+        RemoteViews remoteViews = new RemoteViews(getPackageName(), R.layout.layout_notification);
+        remoteViews.setTextViewText(R.id.msg, "msg from process: " + Process.myPid());
+        remoteViews.setImageViewResource(R.id.icon, R.drawable.ic_dysania);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, DemoActivity.class), PendingIntent.FLAG_UPDATE_CURRENT);
+        remoteViews.setOnClickPendingIntent(R.id.open_activity, pendingIntent);
+
+        Intent intent = new Intent(ACTION_REMOTE_VIEWS);
+        intent.putExtra(EXTRA_REMOTE_VIEWS, remoteViews);
+        sendBroadcast(intent);
+    }
 }
--- chapter05/src/main/java/com/dysania/artofandroid/chapter05/MainActivity.java
+++ chapter05/src/main/java/com/dysania/artofandroid/chapter05/MainActivity.java
@@ -3,18 +3,35 @@ package com.dysania.artofandroid.chapter05;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.NotificationCompat;
 import android.support.v7.app.NotificationCompat.Builder;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.LinearLayout;
 import android.widget.RemoteViews;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements OnClickListener {
 
+    private LinearLayout mLlRemoteViewsContainer;
     private NotificationManager mNotificationManager;
+
+    private BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Toast.makeText(context, "Receive remote msg", Toast.LENGTH_SHORT).show();
+            RemoteViews remoteViews = intent.getParcelableExtra(DemoActivity.EXTRA_REMOTE_VIEWS);
+            if (remoteViews != null) {
+                updateUI(remoteViews);
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +40,14 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
 
         findViewById(R.id.btn_default).setOnClickListener(this);
         findViewById(R.id.btn_custom).setOnClickListener(this);
+        findViewById(R.id.btn_demo).setOnClickListener(this);
+
+        mLlRemoteViewsContainer = (LinearLayout) findViewById(R.id.ll_remote_views);
 
         mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        IntentFilter intentFilter = new IntentFilter(DemoActivity.ACTION_REMOTE_VIEWS);
+        registerReceiver(mBroadcastReceiver, intentFilter);
     }
 
     @Override
@@ -35,6 +58,9 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
                 break;
             case R.id.btn_custom:
                 showCustomNotification();
+                break;
+            case R.id.btn_demo:
+                DemoActivity.actionStart(this);
                 break;
         }
     }
@@ -58,8 +84,9 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
         RemoteViews remoteViews = new RemoteViews(getPackageName(), R.layout.layout_notification);
         remoteViews.setTextViewText(R.id.msg, "Chapter05");                     //更新RemoteViews的文本
         remoteViews.setImageViewResource(R.id.icon, R.mipmap.ic_launcher);      //更新RemoteViews的图片
-        remoteViews.setOnClickPendingIntent(R.id.open_activity, PendingIntent.getActivity(MainActivity.this, 0, new Intent(MainActivity.this, DemoActivity.class),
-                PendingIntent.FLAG_CANCEL_CURRENT));                            //给RemoteViews添加点击事件
+        remoteViews.setOnClickPendingIntent(R.id.open_activity,
+                PendingIntent.getActivity(MainActivity.this, 0, new Intent(MainActivity.this, DemoActivity.class),
+                        PendingIntent.FLAG_CANCEL_CURRENT));                    //给RemoteViews添加点击事件
 
         Notification notification = new Builder(this)
                 .setTicker("Hello World")
@@ -70,5 +97,26 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
                 .build();
 
         mNotificationManager.notify(2, notification);
+    }
+
+    private void updateUI(RemoteViews remoteViews) {
+        //Note: 由于含有ImageView，可能会出现如下错误：AppCompatImageView can't use method with RemoteViews: setImageResource(int)
+        //这时需要把appcompat-v7包调到23.0.1或之前
+
+        //适用于同一个应用的两个进程
+//        View view = remoteViews.apply(this, mLlRemoteViewsContainer);
+//        mLlRemoteViewsContainer.addView(view);
+
+        //适用于不同应用的两个进程
+        int layoutId = getResources().getIdentifier("layout_notification", "layout", getPackageName());
+        View view = getLayoutInflater().inflate(layoutId, mLlRemoteViewsContainer, false);
+        remoteViews.reapply(this, view);
+        mLlRemoteViewsContainer.addView(view);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(mBroadcastReceiver);
     }
 }
