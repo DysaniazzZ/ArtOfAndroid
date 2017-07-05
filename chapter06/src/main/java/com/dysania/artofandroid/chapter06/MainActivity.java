51d69aa6b20d13302c6f4a44e50064303c1aa02b
DysaniazzZ <William.fzy@foxmail.com>
2017-07-05 16:03:20 +0800

    chapter06: the usage of transition drawable

--- chapter06/src/main/java/com/dysania/artofandroid/chapter06/MainActivity.java
+++ chapter06/src/main/java/com/dysania/artofandroid/chapter06/MainActivity.java
@@ -1,7 +1,10 @@
 package com.dysania.artofandroid.chapter06;
 
+import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.tv_transition).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TransitionDrawable drawable = (TransitionDrawable) v.getBackground();
+                drawable.startTransition(3000);
+            }
+        });
     }
 }
