d7fc6a8dcf4f8766d1aaad65995c338b744a8e6d
DysaniazzZ <William.fzy@foxmail.com>
2017-07-19 16:13:10 +0800

    chapter13: the usage of multi dex support

--- chapter13/src/main/java/com/dysania/artofandroid/chapter13/MyApplication.java
+++ chapter13/src/main/java/com/dysania/artofandroid/chapter13/MyApplication.java
@@ -1,12 +1,21 @@
 package com.dysania.artofandroid.chapter13;
 
 import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
 
 /**
  * Created by DysaniazzZ on 19/07/2017.
  */
 
 public class MyApplication extends Application {
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        //install multi dex support
+        MultiDex.install(this);
+    }
 
     @Override
     public void onCreate() {
