28bbad709ad7bc932d9e43abb9b587c825ca53b5
DysaniazzZ <William.fzy@foxmail.com>
2017-07-20 14:12:05 +0800

    chapter15: the usage of viewstub label

--- chapter15/src/main/java/com/dysania/artofandroid/chapter15/MainActivity.java
+++ chapter15/src/main/java/com/dysania/artofandroid/chapter15/MainActivity.java
@@ -2,6 +2,8 @@ package com.dysania.artofandroid.chapter15;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewStub;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +14,9 @@ public class MainActivity extends AppCompatActivity {
 
 //        findViewById(R.id.root_layout).setBackgroundColor(getResources().getColor(R.color.colorAccent));
         findViewById(R.id.include_layout).setBackgroundColor(getResources().getColor(R.color.colorAccent));
+
+        //加载ViewStub的两种方式
+//        findViewById(R.id.viewstub_layout).setVisibility(View.VISIBLE);
+        View inflateView = ((ViewStub)findViewById(R.id.viewstub_layout)).inflate();
     }
 }
