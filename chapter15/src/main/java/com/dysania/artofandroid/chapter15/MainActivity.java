0230c63974e90a4af40e7fd0b15199c4164be035
DysaniazzZ <William.fzy@foxmail.com>
2017-07-20 11:04:39 +0800

    chapter15: the usage of include label

---
+++ chapter15/src/main/java/com/dysania/artofandroid/chapter15/MainActivity.java
@@ -0,0 +1,16 @@
+package com.dysania.artofandroid.chapter15;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        findViewById(R.id.ll_title_layout).setBackgroundColor(getResources().getColor(R.color.colorAccent));
+        findViewById(R.id.include_title_layout).setBackgroundColor(getResources().getColor(R.color.colorAccent));
+    }
+}
